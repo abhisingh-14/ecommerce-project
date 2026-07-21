17a5b76c374796b7e4181c174a904ffc53fecaf7
Abhinav Singh <abhinavsingh14052005@gmail.com>
2026-07-21 21:37:24 +0530

    added tests for PaymentSummary and HomePage

--- ecommerce-frontend/src/pages/home/HomePage.test.jsx
+++ ecommerce-frontend/src/pages/home/HomePage.test.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
-// import Product from './Product';
-// import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 import HomePage from './HomePage';
 
 vi.mock('axios');
 
-
 describe('HomePage component', () => {
   let loadCart;
+  let user;
 
   beforeEach(() => {
+    vi.clearAllMocks();
     loadCart = vi.fn();
 
     axios.get.mockImplementation(async (urlPath) => {
@@ -44,6 +43,8 @@ describe('HomePage component', () => {
         };
       }
     });
+
+    user = userEvent.setup();
   });
 
   it('displays the products correct', async () => {
@@ -67,5 +68,73 @@ describe('HomePage component', () => {
       within(productContainers[1])
         .getByText('Intermediate Size Basketball')
     ).toBeInTheDocument();
-  })
-})
+  });
+
+  it('adds a product to the cart', async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+    const productContainers = await screen.findAllByTestId('product-container');
+
+    const addToCartButton1 = within(productContainers[0])
+      .getByTestId('add-to-cart-button');
+    await user.click(addToCartButton1);
+
+    const addToCartButton2 = within(productContainers[1])
+      .getByTestId('add-to-cart-button');
+    await user.click(addToCartButton2);
+
+    expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1
+    });
+
+    expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1
+    });
+
+    expect(loadCart).toHaveBeenCalledTimes(2);
+  });
+
+  it('selects a quantity', async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+    const productContainers = await screen.findAllByTestId('product-container');
+
+    const quantitySelector1 = within(productContainers[0])
+      .getByTestId('product-quantity-selector');
+    await user.selectOptions(quantitySelector1, '2');
+    expect(quantitySelector1).toHaveValue('2');
+
+    const addToCartButton1 = within(productContainers[0])
+      .getByTestId('add-to-cart-button');
+    await user.click(addToCartButton1);
+
+    const quantitySelector2 = within(productContainers[1])
+      .getByTestId('product-quantity-selector');
+    await user.selectOptions(quantitySelector2, '3');
+    expect(quantitySelector2).toHaveValue('3');
+
+    const addToCartButton2 = within(productContainers[1])
+      .getByTestId('add-to-cart-button');
+    await user.click(addToCartButton2);
+
+    expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2
+    });
+
+    expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 3
+    });
+
+    expect(loadCart).toHaveBeenCalledTimes(2);
+  });
+});
