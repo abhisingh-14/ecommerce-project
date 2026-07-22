c390467dcccbec3b36c22bf0d24c232c2512e1d9
Abhinav Singh <abhinavsingh14052005@gmail.com>
2026-07-22 23:42:53 +0530

    Test for Delivery Options

--- ecommerce-frontend/src/pages/checkout/DeliveryOptions.jsx
+++ ecommerce-frontend/src/pages/checkout/DeliveryOptions.jsx
@@ -20,12 +20,15 @@ function DeliveryOptions({ deliveryOptions, cartItem, loadCart }) {
 
         return (
           <div key={deliveryOption.id} className="delivery-option"
-            onClick={updateDeliveryoption}>
+            onClick={updateDeliveryoption}
+            data-testid="delivery-option">
             <input type="radio"
               checked={deliveryOption.id === cartItem.deliveryOptionId}
               onChange={() => { }}
               className="delivery-option-input"
-              name={`delivery-option-${cartItem.productId}`} />
+              name={`delivery-option-${cartItem.productId}`}
+              data-testid="delivery-option-input"
+            />
             <div>
               <div className="delivery-option-date">
                 {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
